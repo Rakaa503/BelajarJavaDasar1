856bfbbbcaa329c0ff2ff66f8cd8b76db5df306d
Rakha Avilla <azmyervawanrakhaavilla@gmail.com>
2026-03-27 14:36:09 +0700

    Update Class Perbandingan Nilai

--- src/main/java/com/mycompany/belajardasarjava1/PerbandinganNilai.java
+++ src/main/java/com/mycompany/belajardasarjava1/PerbandinganNilai.java
@@ -4,10 +4,33 @@
  */
 package com.mycompany.belajardasarjava1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ASUS
  */
 public class PerbandinganNilai {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int nilai1, nilai2;
+        
+        System.out.print("Masukan Nilai Pertama: ");
+        nilai1 = input.nextInt();
+        
+        System.out.print("Masukan Nilai Pertama: ");
+        nilai2 = input.nextInt();
+        
+        System.out.println("\n---Hasil Perbandingan ---");
+        // Hasilnya akan boolean (true/false)
+        System.out.println("Apakah Nilai 1 == Nilai 2? " + (nilai1 == nilai2));
+        System.out.println("Apakah Nilai 1 >  Nilai 2? " + (nilai1 > nilai2));
+        System.out.println("Apakah Nilai 1 != Nilai 2? " + (nilai1 != nilai2));
+                
+        input.close();    
+        
+    }
     
 }
