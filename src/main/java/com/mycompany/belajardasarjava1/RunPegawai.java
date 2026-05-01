56413b726d371533699f5bbf05b975cf0daf474f
Rakha Avilla <azmyervawanrakhaavilla@gmail.com>
2026-05-01 14:27:05 +0700

    Tugas Pertemuan 11

---
+++ src/main/java/com/mycompany/belajardasarjava1/RunPegawai.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajardasarjava1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class RunPegawai {
+      public static void main(String[] args) {
+          // 1. Buat Object Pegawai biasa
+          Pegawai pgw = new Pegawai("Anton", 3000000);
+          System.out.println("== Pegawai Biasa ==");
+          pgw.tampilInfo();
+          
+          System.out.println("\n");
+          
+         // 2. Buat Object Manajer (turunan Pegawai)
+         Manajer mnr = new Manajer("Budi", 5000000, 2000000);
+         System.out.println("== Pegawai Manajer ==");
+         mnr.tampilInfo(); // Memanggil method yang di-Override
+         
+      }
+      
+}
+   
+
