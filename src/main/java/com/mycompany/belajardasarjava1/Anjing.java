504c3aff25a4552588a1291c896a727b3657f95b
Rakha Avilla <azmyervawanrakhaavilla@gmail.com>
2026-05-14 18:45:37 +0700

    Tugas Java Pertemuan 12

---
+++ src/main/java/com/mycompany/belajardasarjava1/Anjing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajardasarjava1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Anjing extends Hewan {
+    @Override
+    public void bersuara() {
+        System.out.println("Guk...Guk...");
+}
+
+
+}
---
+++ src/main/java/com/mycompany/belajardasarjava1/Kucing.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajardasarjava1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kucing extends Hewan {
+     @Override
+     public void bersuara() {
+    System.out.println("Meong...Meong...");
+}
+     
+}
+
+    
+
